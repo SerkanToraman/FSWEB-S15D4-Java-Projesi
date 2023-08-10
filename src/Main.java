04facb25aea43f96fba397ac975399a8ab60dd78
serkantoraman <serkan@github.com>
2023-08-10 15:55:13 +0300

    Task 1 is done

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -9,7 +11,24 @@ public class Main {
 
     }
 
-    public static boolean checkForPalindrome(String text) {
-        return false;
+    public static String checkForPalindrome(String text) {
+        String cleanedText = text.replaceAll("[.,!?\"]", "");
+        String textWithoutGaps = cleanedText.replaceAll("\\s+", "");
+        char[] letters = textWithoutGaps.toLowerCase().toCharArray();
+        Stack<Character> stack = new Stack<>();
+
+        for (int i =0; i<letters.length/2;i++) {
+            stack.push(letters[i]);
+        }
+        int midIndex = (letters.length % 2 == 0) ? letters.length / 2 : letters.length / 2 + 1;
+        for (int i = midIndex; i < letters.length; i++) {
+            if (letters[i] != stack.pop()) {
+                String result = "\""+text + "\""+" is NOT a palindrom";
+                return result;
+            }
+        }
+
+        String result = "\""+text + "\""+" is a palindrom";
+        return result;
     }
 }
